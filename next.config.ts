cbee4876359ce8637d0ed10f461c85b3f815bfc7
bsjuuny <bsjuuny@naver.com>
2026-02-24 21:21:51 +0900

    Fix FTP deploy 550 error by fixing build ID and sync-state

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  generateBuildId: async () => 'my-build-id',
   reactCompiler: true,
   basePath: '/trendingrankings',
   images: {
